12aaa19ffeb04a6a3fcd7aa96b64edc81701b6c5
Botking134 <167695470+Botking134@users.noreply.github.com>
2026-06-29 22:15:05 +0100

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -1,5 +1,5 @@
 // plugins/user/user.js
-const config = require('../../config');
+const config = require('../config');
 const { normalizeToJid } = require('../../stateManager');
 const { DEV_LIDS } = require('../devs');
 
